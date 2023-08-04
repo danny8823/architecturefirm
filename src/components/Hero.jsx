48a117af54a3e1461a8455e2acdaefeba2834665
danny8823 <dannyyoo714@gmail.com>
2023-08-03 23:36:32 -0700

    added hero section, title, headline and image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import hero_image1 from '../images/hero.jpg'
+import hero_image2 from '../images/hero2.jpg'
 export const Hero = () => {
     return (
-        <div>
-            Hero
-        </div>
+        <section className= 'hero-container'>
+            <img className = 'hero-img' src = {hero_image1} alt = 'downtown buildings'/>
+            <div className = 'hero-title'>
+                <p>Pristine Architecture Firm</p>
+                <p className = 'hero-headline'>Architectural Visionaries Redefining Urban Landscapes</p>
+            </div>
+            
+        </section>
     )
 }
