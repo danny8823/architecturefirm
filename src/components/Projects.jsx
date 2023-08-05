85584e451fb6207243b67ea0abca20fdc82cf401
danny8823 <dannyyoo714@gmail.com>
2023-08-04 18:02:27 -0700

    Added animations to features cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { feature_images } from "./constants";
-import {motion, Variants } from "framer-motion"
-
+import {motion, useAnimation } from "framer-motion"
+import { useInView } from "react-intersection-observer";
 export const Features = () => {
     
+    const control = useAnimation()
+    const [ref, inView] = useInView()
+
+    const variantsOne = {
+        visible: { opacity: 1, scale: 1, transition: {duration: 0.5}},
+        hidden: { opactiy: 0, scale: 0}
+    }
+
+    useEffect(() => {
+        if(inView) {
+            control.start("visible")
+        } else {
+            control.start("hidden")
+        }
+    }, [control , inView])
+
     return (
         <section>
             <div>
                 {feature_images.map(({image}) => (
-                    <div>
+                    <motion.div 
+                    className = 'features'
+                    ref = {ref}
+                    animate = {control}
+                    initial = "hidden"
+                    variants={variantsOne}
+                    >
                         <img className = 'feature-imgs' src = {image} alt = 'projects'/>
-                    </div>
+                    </motion.div>
                 ))}
             </div>
         </section>
